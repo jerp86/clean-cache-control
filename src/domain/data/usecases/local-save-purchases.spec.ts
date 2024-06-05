86d17330df252d39e5243128e74a14e54f1a12a0
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2024-06-05 11:02:26 -0300

    ensure LocalSavePurchases does not delete cache upon creation

---
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -0,0 +1,17 @@
+class LocalSavePurchases {
+  constructor(private readonly cacheStore: ICacheStore) {}
+}
+
+interface ICacheStore {}
+
+class CacheStoreSpy implements ICacheStore {
+  deleteCallsCount = 0;
+}
+
+describe("LocalSavePurchases Suite Tests", () => {
+  test("Should not delete cache on sut.init", () => {
+    const cacheStore = new CacheStoreSpy();
+    new LocalSavePurchases(cacheStore);
+    expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+});
