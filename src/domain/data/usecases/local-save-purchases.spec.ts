9b1c86f09d22741ff6fc83814493486ac3407168
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2024-06-05 11:11:17 -0300

    ensure LocalSavePurchases deletes old cache on save

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,17 +1,35 @@
 class LocalSavePurchases {
   constructor(private readonly cacheStore: ICacheStore) {}
+
+  async save(): Promise<void> {
+    this.cacheStore.delete();
+  }
 }
 
-interface ICacheStore {}
+interface ICacheStore {
+  delete: () => void;
+}
 
 class CacheStoreSpy implements ICacheStore {
   deleteCallsCount = 0;
+
+  delete() {
+    this.deleteCallsCount++;
+  }
 }
 
 describe("LocalSavePurchases Suite Tests", () => {
-  test("Should not delete cache on sut.init", () => {
+  it("should not delete cache on sut.init", () => {
     const cacheStore = new CacheStoreSpy();
     new LocalSavePurchases(cacheStore);
     expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+
+  it("should delete cache on sut.delete", async () => {
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalSavePurchases(cacheStore);
+    await sut.save();
+
+    expect(cacheStore.deleteCallsCount).toBe(1);
   });
 });
