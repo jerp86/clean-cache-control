a13500a16ce7ebb3011c99932b16b56b5b822dcf
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2024-06-05 16:26:56 -0300

    merge tests

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -19,17 +19,6 @@ describe("LocalSavePurchases Suite Tests", () => {
     expect(cacheStore.messages).toEqual([]);
   });
 
-  it("should delete cache on sut.save", async () => {
-    const { cacheStore, sut } = makeSut();
-    await sut.save(mockPurchases());
-
-    expect(cacheStore.messages).toEqual([
-      CacheStoreSpy.Message.delete,
-      CacheStoreSpy.Message.insert,
-    ]);
-    expect(cacheStore.deleteKey).toBe("purchases");
-  });
-
   it("should not insert new Cache if delete fails", async () => {
     const { cacheStore, sut } = makeSut();
     cacheStore.simulateDeleteError();
@@ -49,6 +38,7 @@ describe("LocalSavePurchases Suite Tests", () => {
       CacheStoreSpy.Message.delete,
       CacheStoreSpy.Message.insert,
     ]);
+    expect(cacheStore.deleteKey).toBe("purchases");
     expect(cacheStore.insertKey).toBe("purchases");
     expect(cacheStore.insertValues).toEqual(purchases);
   });
