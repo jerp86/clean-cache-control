83d8d94dbed3eff0d57e4f176d29677422f0207a
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2024-06-06 10:52:35 -0300

    ensure LocalLoadPurchases deletes old cache if load fails

--- src/data/usecases/load-purchases/local-validate-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-validate-purchases.spec.ts
@@ -21,4 +21,18 @@ describe("LocalValidatePurchases Suite Test", () => {
 
     expect(cacheStore.actions).toEqual([]);
   });
+
+  it("should delete cache if load fails", () => {
+    const { cacheStore, sut } = makeSut();
+    cacheStore.simulateFetchError();
+    sut.validate();
+
+    expect(cacheStore.actions).not.toContainEqual(CacheStoreSpy.Action.insert);
+    expect(cacheStore.actions).toContainEqual(CacheStoreSpy.Action.delete);
+    expect(cacheStore.actions).toEqual([
+      CacheStoreSpy.Action.fetch,
+      CacheStoreSpy.Action.delete,
+    ]);
+    expect(cacheStore.deleteKey).toBe(KEY);
+  });
 });
