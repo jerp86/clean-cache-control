4f515257372f1a999f7373dd430ded6a8c0024b5
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2024-06-06 08:58:02 -0300

    ensure LocalLoadPurchases returns empty list on load failure

--- src/data/usecases/load-purchases/local-load-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -27,4 +27,18 @@ describe("LocalLoadPurchases Suite Test", () => {
     expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch]);
     expect(cacheStore.fetchKey).toBe("purchases");
   });
+
+  it("should return empty list if load fails", async () => {
+    const { cacheStore, sut } = makeSut();
+    cacheStore.simulateFetchError();
+    const purchases = await sut.loadAll();
+
+    expect(cacheStore.actions).not.toContainEqual(CacheStoreSpy.Action.insert);
+    expect(cacheStore.actions).toEqual([
+      CacheStoreSpy.Action.fetch,
+      CacheStoreSpy.Action.delete,
+    ]);
+    expect(cacheStore.deleteKey).toBe("purchases");
+    expect(purchases).toEqual([]);
+  });
 });
--- src/domain/usecases/index.ts
+++ src/domain/usecases/index.ts
@@ -1,1 +1,2 @@
+export * from "./load-purchases";
 export * from "./save-purchases";
